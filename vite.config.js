f326e846a2380c90d0ecd93f1c50eecccdb3e4fd
SrJacoby <luizjacoby294@gmail.com>
2025-06-12 14:33:25 -0300

    arrumando configuracoes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/modo-escuro-react/', // Substitua pelo nome do seu repositório
+  base: '/',
 });
